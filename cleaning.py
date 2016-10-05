0a54e104ef094f5a676a066544c1134b04651565
ray0be <victor.paysaut@gmail.com>
2016-10-05 13:02:10 +0200

    cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,20 @@
 import csv
 import matplotlib.pyplot as plt
 
+with open('movie_metadata.csv', newline='') as csv_file:
+    metadata_reader = csv.reader(csv_file)
+    next(metadata_reader)
 
+    budget = []
+    ratings = []
+    directors = []
+
+    for row in metadata_reader:
+        if row[22] == "" or row[25] == "":
+            continue
+
+        if float(row[22]) > 500000000:
+            continue
+
+        budget.append(float(row[22]))
+        ratings.append(float(row[25]))
