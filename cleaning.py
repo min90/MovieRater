d3fca88c2e4bf1b746cf7e30a152aaee810e662b
ray0be <victor.paysaut@gmail.com>
2016-10-05 14:11:08 +0200

    update for cleaning process

--- cleaning.py
+++ cleaning.py
@@ -1,20 +1,26 @@
 import csv
-import matplotlib.pyplot as plt
 
-with open('movie_metadata.csv', newline='') as csv_file:
-    metadata_reader = csv.reader(csv_file)
-    next(metadata_reader)
+# Cleaning process
 
-    budget = []
-    ratings = []
-    directors = []
+with open('cleaned_data.csv', 'w') as newfile:
+    writer = csv.writer(newfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Director', 'Actor1', 'Actor2', 'Actor3', 'Rating'])
+    
+    with open('movie_metadata.csv', 'r') as oldfile:
+        reader = csv.reader(oldfile)
+        next(reader)
 
-    for row in metadata_reader:
-        if row[22] == "" or row[25] == "":
-            continue
+        # director = 1
+        # actor 1 = 10
+        # actor 2 = 6
+        # actor 3 = 14
+        # rating = 25
 
-        if float(row[22]) > 500000000:
-            continue
+        for row in reader:
+            if row[1]=="" or row[10]=="" or row[6]=="" or row[14]=="" or row[25]=="":
+                continue
+            
+            # saving into the new csv file
+            writer.writerow([row[1], row[10], row[6], row[14], float(row[25])])
 
-        budget.append(float(row[22]))
-        ratings.append(float(row[25]))
+print "Done."
