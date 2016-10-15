7537550af80d25842caeef6ca88f29337fc68dea
ray0be <victor.paysaut@gmail.com>
2016-10-15 22:25:09 +0200

    Made simpler

--- Persistence/Reader.py
+++ Persistence/Reader.py
@@ -5,6 +5,8 @@ class CSVReader:
     @staticmethod
     def read(file_path):
         data = []
+        nb_directors = 0
+        nb_actors = 0
 
         with open(file_path, 'r', encoding="utf8") as newfile:
             reader = csv.reader(newfile)
@@ -15,6 +17,16 @@ class CSVReader:
                 if not header:
                     data.append(row)
 
+                    id_dir = int(row[4])
+                    id_1 = int(row[5])
+                    id_2 = int(row[6])
+                    id_3 = int(row[7])
+
+                    if id_dir > nb_directors:
+                        nb_directors = id_dir
+                    if max(id_1, id_2, id_3) > nb_actors:
+                        nb_actors = max(id_1, id_2, id_3)
+
                 header = False
 
-        return data
+        return data, (nb_directors+1), (nb_actors+1)
