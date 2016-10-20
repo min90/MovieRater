d4981e90ddfe7831b49edbc56ec1010a75dc87ea
svensgaard <hypermads@gmail.com>
2016-10-20 20:35:44 +0200

    Reader

--- Persistence/Reader.py
+++ Persistence/Reader.py
@@ -5,46 +5,28 @@ class CSVReader:
     @staticmethod
     def read(file_path):
         data = []
-        directors = []
-        actors = []
+        nb_directors = 0
+        nb_actors = 0
 
         with open(file_path, 'r', encoding="utf8") as newfile:
             reader = csv.reader(newfile)
 
-            # reading the file
             header = True
             for row in reader:
 
                 if not header:
                     data.append(row)
 
-                    if row[0] not in directors:
-                        directors.append(row[0])
+                    id_dir = int(row[4])
+                    id_1 = int(row[5])
+                    id_2 = int(row[6])
+                    id_3 = int(row[7])
 
-                    if row[1] not in actors:
-                        actors.append(row[1])
-                    if row[2] not in actors:
-                        actors.append(row[2])
-                    if row[3] not in actors:
-                        actors.append(row[3])
+                    if id_dir > nb_directors:
+                        nb_directors = id_dir
+                    if max(id_1, id_2, id_3) > nb_actors:
+                        nb_actors = max(id_1, id_2, id_3)
 
                 header = False
 
-        return directors, actors, data
-
-    # In the method below, the "array" is the list of directors or actors
-    # The method search for the given name in the array and get the position
-    # Then it creates an array (length : directors or actors number) with zeros and a one at the right position
-    @staticmethod
-    def getVector(array, name):
-        vector = []
-        length = len(array)
-        position = array.index(name);
-
-        for i in range(0, length):
-            if i == position:
-                array.append(1)
-            else:
-                array.append(0)
-
-        return array
+        return data, (nb_directors+1), (nb_actors+1)
