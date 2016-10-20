0c2613dc17f34edf7b321ac260ade8f0168530fa
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-10-20 18:31:58 +0200

    Getting actor, and director id

--- KNN/Knn.py
+++ KNN/Knn.py
@@ -15,11 +15,31 @@ class Knn:
 
         return vector
 
+    def getting_actor(self, actor_name):
+        for movie in self.data:
+            if actor_name in movie and actor_name not in movie[0]:
+                index = movie.index(actor_name)
+                actor_id = movie[index + 4]
+                return actor_id
+            else:
+                continue
+
+    def getting_director(self, director_name):
+        for movie in self.data:
+            if director_name in movie[0]:
+                director_id = movie[4]
+                return director_id
+            else:
+                continue
+
     # testing
     def test(self):
         print("directors : " + str(self.nb_directors))
         print("actors : " + str(self.nb_actors))
         print(self.getVector(2, "actor"))
+        print(self.data[0][6])
+        print(self.getting_actor("Clint Eastwood"))
+        print(self.getting_director("Clint Eastwood"))
 
 knn = Knn()
 knn.test()
