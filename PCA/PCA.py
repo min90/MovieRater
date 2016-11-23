972e65c824c3882136f22f084d8c2bd791681227
ray0be <victor.paysaut@gmail.com>
2016-11-23 16:32:42 +0100

    PCA version 2

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -43,14 +43,14 @@ for i in range(0, nRows):
     act1Likes = int(data[i][6])
     act2Likes = int(data[i][7])
     act3Likes = int(data[i][8])
-    #rating = float(data[i][4])
     row = [dirLikes, act1Likes, act2Likes, act3Likes]
     Dataset.append(row)
 
 Dataset = np.array(Dataset)
+Dts = Dataset.T
 
 print("\n~~~~~ Dataset ~~~~~")
-print("Dataset shape : " + str(Dataset.shape))
+print("Dts shape : " + str(Dts.shape))
 
 ###############################################################################
 ###
@@ -179,46 +179,51 @@ if performMlabPCA == True:
 ###############################################################################
 
 if performManualPCA == True:
-    print("\n~~~~~ Manual PCA ~~~~~")
+    print("\nManual PCA.")
 
     # ===============================================
     # Computing the d-dimensional mean vector
 
-    mean_dir = np.mean(Dataset[:, 0])
-    mean_act1 = np.mean(Dataset[:, 1])
-    mean_act2 = np.mean(Dataset[:, 2])
-    mean_act3 = np.mean(Dataset[:, 3])
-    #mean_rating = np.mean(Dataset[:, 4])
+    print('\n~~~~~ Mean vector ~~~~~')
 
+    mean_dir = np.mean(Dts[0, :])
+    mean_act1 = np.mean(Dts[1, :])
+    mean_act2 = np.mean(Dts[2, :])
+    mean_act3 = np.mean(Dts[3, :])
     mean_vector = np.array( [ [mean_dir], [mean_act1], [mean_act2], [mean_act3] ] )
 
-    print('DirectorLikes: \tMin: ' + str(np.min(Dataset[:, 0])) + '\tMax: ' + str(np.max(Dataset[:, 0])) + '\tStandard deviation: ' + str(np.std(Dataset[:, 0])) + '\tMean: ' + str(mean_dir))
-    print('Actor-1-Likes: \tMin: ' + str(np.min(Dataset[:, 1])) + '\tMax: ' + str(np.max(Dataset[:, 1])) + '\tStandard deviation: ' + str(np.std(Dataset[:, 1])) + '\tMean: ' + str(mean_act1))
-    print('Actor-2-Likes: \tMin: ' + str(np.min(Dataset[:, 2])) + '\tMax: ' + str(np.max(Dataset[:, 2])) + '\tStandard deviation: ' + str(np.std(Dataset[:, 2])) + '\tMean: ' + str(mean_act2))
-    print('Actor-3-Likes: \tMin: ' + str(np.min(Dataset[:, 3])) + '\tMax: ' + str(np.max(Dataset[:, 3])) + '\tStandard deviation: ' + str(np.std(Dataset[:, 3])) + '\tMean: ' + str(mean_act3))
+    print('DirectorLikes: \tMin: ' + str(np.min(Dts[0, :])) + '\tMax: ' + str(np.max(Dts[0, :])) + '\tStandard deviation: ' + str(np.std(Dts[0, :])) + '\tMean: ' + str(mean_dir))
+    print('Actor-1-Likes: \tMin: ' + str(np.min(Dts[1, :])) + '\tMax: ' + str(np.max(Dts[1, :])) + '\tStandard deviation: ' + str(np.std(Dts[1, :])) + '\tMean: ' + str(mean_act1))
+    print('Actor-2-Likes: \tMin: ' + str(np.min(Dts[2, :])) + '\tMax: ' + str(np.max(Dts[2, :])) + '\tStandard deviation: ' + str(np.std(Dts[2, :])) + '\tMean: ' + str(mean_act2))
+    print('Actor-3-Likes: \tMin: ' + str(np.min(Dts[3, :])) + '\tMax: ' + str(np.max(Dts[3, :])) + '\tStandard deviation: ' + str(np.std(Dts[3, :])) + '\tMean: ' + str(mean_act3))
     print('Mean vector generated.')
 
     # ===============================================
     # Computing the Scatter Matrix
 
-    scatter_matrix = np.zeros((5, 5))
+    print('\n~~~~~ Scatter matrix ~~~~~')
 
-    for i in range(Dataset.shape[0]):
-        scatter_matrix += (Dataset[i,:].reshape(1,5) - mean_vector).dot((Dataset[i,:].reshape(1,5) - mean_vector))
+    scatter_matrix = np.zeros((4, 4))
 
-    print('Scatter Matrix :\n', scatter_matrix)
+    for i in range(Dts.shape[1]):
+        scatter_matrix += (Dts[:,i].reshape(4,1) - mean_vector).dot((Dts[:,i].reshape(4,1) - mean_vector).T)
+
+    print('Scatter matrix generated.')
+    print(scatter_matrix)
 
     # ===============================================
     # Computing the Covariance Matrix
 
-    cov_mat = np.cov([Dataset[:,0], Dataset[:,1], Dataset[:,2], Dataset[:,3], Dataset[:,4]])
-    print('Covariance Matrix :\n', cov_mat)
+    print('\n~~~~~ Covariance matrix ~~~~~')
+
+    cov_mat = np.cov( [ Dts[0,:], Dts[1,:], Dts[2,:], Dts[3,:] ] )
+    print('Covariance matrix generated.')
+    print(cov_mat)
 
     # ===============================================
     # Computing eigenvectors and corresponding eigenvalues
 
-    print('\n' + 50 * '=')
-    print("Eigenvectors & Eigenvalues\n")
+    print('\n~~~~~ Eigen vectors & Eigen values ~~~~~')
 
     # eigenvectors and eigenvalues from the scatter matrix
     eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
@@ -227,45 +232,103 @@ if performManualPCA == True:
     eig_val_cov, eig_vec_cov = np.linalg.eig(cov_mat)
 
     for i in range(len(eig_val_sc)):
-        eigvec_sc = eig_vec_sc[i,:].reshape(5,1)
-        eigvec_cov = eig_vec_cov[i,:].reshape(5,1)
+        eigvec_sc = eig_vec_sc[:,i].reshape(1,4).T
+        eigvec_cov = eig_vec_cov[:,i].reshape(1,4).T
         assert eigvec_sc.all() == eigvec_cov.all(), 'Eigenvectors are not identical'
 
+        print(30*"*")
         print('\tEigenvector {} : \n{}'.format(i+1, eigvec_sc))
         print('\tEigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
         print('\tEigenvalue {} from covariance matrix: {}'.format(i+1, eig_val_cov[i]))
         print('\tScaling factor: ', eig_val_sc[i]/eig_val_cov[i])
-        print('\t' + 40 * '-')
 
     # ===============================================
     # Checking eigen vectors/values computation
 
-    for i in range(len(eig_val_sc)):
-        eigv = eig_vec_sc[i,:].reshape(5,1)
-        np.testing.assert_array_almost_equal(scatter_matrix.dot(eigv), eig_val_sc[i] * eigv, decimal=6, err_msg='', verbose=True)
+    print('\n~~~~~ Eigen vectors/values computation check ~~~~~')
+
+    for i in range(len(eig_val_cov)):
+        eigv = eig_vec_cov[:,i].reshape(1,4).T
+        np.testing.assert_array_almost_equal(cov_mat.dot(eigv), eig_val_cov[i] * eigv, decimal=6, err_msg='ERROR', verbose=True)
+        print('Eigen vector/value #{} OK.'.format(i+1))
+
+    print('Checking done.')
 
     # ===============================================
     # Sorting the eigenvectors by decreasing eigenvalues
 
+    print('\n~~~~~ Eigen vectors sorting ~~~~~')
+
+    # Just check if the eigen vectors have all the same unit length (1)
     for ev in eig_vec_sc:
         np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
 
     # Make a list of (eigenvalue, eigenvector) tuples
-    eig_pairs = [(np.abs(eig_val_sc[i]), eig_vec_sc[i,:]) for i in range(len(eig_val_sc))]
+    eig_pairs = [(np.abs(eig_val_cov[i]), eig_vec_cov[:,i]) for i in range(len(eig_val_cov))]
 
     # Sort the (eigenvalue, eigenvector) tuples from high to low
     eig_pairs.sort(key=lambda x: x[0], reverse=True)
 
-    # Visually confirm that the list is correctly sorted by decreasing eigenvalues
-    for i in eig_pairs:
-        print(i[0])
+    print('Vectors are now sorted. Let\'s check with the eigenvalues order :')
+
+    # Visually check that the list is correctly sorted by decreasing eigenvalues
+    for pair in eig_pairs:
+        print(pair[0])
 
     # ===============================================
-    # Choosing k eigenvectors with the largest eigenvalues
+    # Ploting cumulative sum of explained variance of each PC
 
-    #matrix_w = np.hstack((eig_pairs[0][1].reshape(5,1), eig_pairs[1][1].reshape(5,1)))
-    #print('Matrix W :\n', matrix_w)
+    print('\n~~~~~ How many PCs do we need ? ~~~~~')
 
+    PC = []
+    PC.append([0, 0])
+    sum = 0
+    sum_eigenvalues = np.sum(eig_val_cov)
+
+    print('Eigenvalue sum : ' + str(sum_eigenvalues))
+
+    for k in range(len(eig_val_cov)):
+        sum += (eig_val_cov[k] / sum_eigenvalues)*100
+        PC.append([k+1, sum])
+
+    PC = np.array(PC)
+    print('Cumulative sum of explained variances : (%)')
+    print(PC)
+
+    plt.plot(PC[:,0], PC[:,1], '-o', markersize=3, color='blue', alpha=1, label='whole dataset')
+    plt.xlabel('Number of PCs')
+    plt.ylabel('Cumulative variance (%)')
+    plt.xlim([0, 4])
+    plt.ylim([0, 100])
+    plt.xticks(np.arange(0, 5, 1))
+    plt.title('Amount of variance explained by PCs')
+    print("Graph displayed.")
+    plt.show()
+
+    # ===============================================
+    # Choosing 1 eigenvector with the largest eigenvalue
+
+    print('\n~~~~~ Transformation matrix ~~~~~')
+    matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1), eig_pairs[1][1].reshape(4,1)))
+    print('Matrix W : (' + str(matrix_w.shape) + ')\n', matrix_w)
+
+    # ===============================================
     # Transforming the samples onto the new subspace
-    #transformed = matrix_w.dot(Dataset)
-    #print(matrix_w.shape)
+
+    print('\n~~~~~ Data transformation ~~~~~')
+    transformed = matrix_w.T.dot(Dts)
+    print('Data transformed. (' + str(transformed.shape) + ')\n', transformed)
+    print('Done.')
+
+    # ===============================================
+    # Final plot
+
+    print('\n~~~~~ Final plot Feature/Feature ~~~~~')
+    plt.plot(transformed[0,:], transformed[1,:], '-o', markersize=3, color='green', alpha=1, label='whole dataset')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.xlim([-10000, 700000])
+    plt.ylim([-75000, 125000])
+    plt.title('PCA work visualization')
+    print("Graph displayed.")
+    plt.show()
